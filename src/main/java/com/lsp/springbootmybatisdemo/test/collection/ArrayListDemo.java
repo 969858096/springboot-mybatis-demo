64072b96eebdb3d85e45460b82f6c26327e20306
lsp <969858096@qq.com>
2020-01-03 00:24:32 +0800

    并发编程5

---
+++ src/main/java/com/lsp/springbootmybatisdemo/test/collection/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.lsp.springbootmybatisdemo.test.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @FileName: ArrayListDemo
+ * @Description:
+ * @AuthOr: lsp
+ * @Date: 2020/1/2 20:04
+ */
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        List l = new ArrayList();
+        List l1 = new ArrayList(10);
+    }
+}
